d86410129b87f98857d7e5fc085dea4bb4d4365c
Janson Kang <temasek71@gmail.com>
2014-02-15 03:23:07 +0800

    hlte: Enable temasek unofficial build tag

--- cm.mk
+++ cm.mk
@@ -9,5 +9,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Custom unofficial build tag
+TARGET_UNOFFICIAL_BUILD_ID := temasek
+
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := cm_hlte
